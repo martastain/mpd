1eaa0290a3063ef4f060a1605a2bd6f6c98823c7
Martastain <martas@imm.cz>
2017-01-27 01:03:30 +0100

    xml_attrs now uses dicts as source

--- mpd/utils.py
+++ mpd/utils.py
@@ -1,6 +1,5 @@
 def indent(src, l=4):
-    return "\n".join(["{}{}\n".format(l*" ", s) for s in src.split("\n")])
-
+    return "\n".join(["{}{}\n".format(l*" ", s) for s in src.strip().split("\n")])
 
 def xml_attrs(attrs):
-    return " ".join(["{}=\"{}\"".format(*attr) for attr in attrs if attr[1] is not None])
+    return " ".join(["{}=\"{}\"".format(key, attrs[key]) for key in attrs if attrs[key] is not None])
