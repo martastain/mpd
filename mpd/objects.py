6dfd82791a729e7dcb7e98d1e580c9c6dd4f19c6
Martastain <martas@imm.cz>
2017-01-29 00:23:32 +0100

    use mk_xml_tag function

--- mpd/objects.py
+++ mpd/objects.py
@@ -3,31 +3,61 @@ from .common import *
 __all__ = ["SegmentTemplate", "Representation", "AdaptationSet", "Period"]
 
 
-
 class SegmentTemplate(BaseObject):
-    keys = ["media", "index", "initialization", "bitstreamSwitching", "duration", "startNumber"]
+    keys = [
+            "media",
+            "index",
+            "initialization",
+            "bitstreamSwitching"
+        ] + MULTIPLE_SEGMENT_BASE_TYPE_KEYS
 
     @property
     def xml(self):
-        return "<SegmentTemplate {}/>".format(xml_attrs(self.attrs))
+        return "<SegmentTemplate {}/>".format(mk_xml_attrs(self.attrib))
 
 
 class Representation(BaseObject):
+    keys = [
+            "id",
+            "bandwidth",
+            "qualityRanking",
+            "dependencyId",
+            "mediaStreamStructureId"
+        ] + REPRESENTATION_BASE_TYPE_KEYS
+
     def __init__(self, parent, **kwargs):
         super(Representation, self).__init__(parent, **kwargs)
         self.segment_template = None
 
     @property
     def xml(self):
-        result = "<Representation {}>\n".format(xml_attrs(self.attrs))
+        content = []
         if self.segment_template:
-            result += "\n    " + self.segment_template.xml + "\n"
-        result += "</Representation>"
-        return result
+            content = [self.segment_template.xml]
+        return mk_xml_tag("Representation", self.attrib, content)
 
 
 class AdaptationSet(BaseObject):
-    keys = []
+    keys = [
+            "id",
+            "group",
+            "lang",
+            "contentType",
+            "par",
+            "minBandwidth",
+            "maxBandwidth",
+            "minWidth",
+            "maxWidth",
+            "minHeight",
+            "maxHeight",
+            "minFrameRate",
+            "maxFrameRate",
+            "segmentAlignment",
+            "subsegmentAlignment",
+            "subsegmentStartsWithSAP",
+            "bitstreamSwitching",
+        ] + REPRESENTATION_BASE_TYPE_KEYS
+
     defaults = {}
 
     def __init__(self, parent, **kwargs):
@@ -37,13 +67,18 @@ class AdaptationSet(BaseObject):
 
     @property
     def xml(self):
-        result = "<AdaptationSet {}>\n".format(xml_attrs(self.attrs))
+        content = []
         if self.segment_template:
-            result += indent(self.segment_template.xml)
+            content.append(self.segment_template.xml)
         for representation in self.representations:
-            result += indent(representation.xml)
-        result += "</AdaptationSet>"
-        return result
+            content.append(representation.xml)
+        return mk_xml_tag("AdaptationSet", self.attrib, content)
+
+    def add_representation(self, **kwargs):
+        if not "id" in kwargs:
+            kwargs["id"] = len(self.adaptation_sets)
+        self.representations.append(Representation(self, **kwargs))
+        return self.representations[-1]
 
 
 class Period(BaseObject):
@@ -51,18 +86,15 @@ class Period(BaseObject):
 
     def __init__(self, parent, **kwargs):
         super(Period, self).__init__(parent, **kwargs)
-        self.attrs = [
-            ["id",       id],
-            ["start",    "PT0S"]
-        ]
         self.adaptation_sets = []
 
     @property
     def xml(self):
-        result = "<Period {}>\n".format(xml_attrs(self.attrs))
-        for adaptation_set in self.adaptation_sets:
-            result += indent(adaptation_set.xml)
-        result += "</Period>"
-        return result
+        return mk_xml_tag("Period", self.attrib, [c.xml for c in self.adaptation_sets])
 
+    def add_adaptation_set(self, **kwargs):
+        if not "id" in kwargs:
+            kwargs["id"] = len(self.adaptation_sets)
+        self.adaptation_sets.append(AdaptationSet(self, **kwargs))
+        return self.adaptation_sets[-1]
 
