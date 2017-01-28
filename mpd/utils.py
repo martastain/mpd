5ab755d505bdb43e020e7a017524ebc92182af32
Martastain <martas@imm.cz>
2017-01-28 01:22:15 +0100

    added mk_tag helper function, indent rewritten

--- mpd/utils.py
+++ mpd/utils.py
@@ -1,5 +1,24 @@
 def indent(src, l=4):
-    return "\n".join(["{}{}\n".format(l*" ", s) for s in src.strip().split("\n")])
+    result = ""
+    for line in src.split("\n"):
+       result += l * " "
+       result += line
+       result += "\n"
+    return result
+
 
 def xml_attrs(attrs):
     return " ".join(["{}=\"{}\"".format(key, attrs[key]) for key in attrs if attrs[key] is not None])
+
+
+def mk_tag(name, attrs, contents=[]):
+    result = "<{}".format(name)
+    if attrs:
+        result += " " + xml_attrs(attrs)
+    if not contents:
+        result += "/>"
+    else:
+        result += ">\n"
+        result += indent("\n".join(contents) )
+        result += "</{}>".format(name)
+    return result
