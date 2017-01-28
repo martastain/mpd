7315359d8225a5b48f39d5dc99b894de2cfb51fd
Martastain <martas@imm.cz>
2017-01-29 00:23:02 +0100

    base object in common

---
+++ mpd/common.py
@@ -0,0 +1,28 @@
+from .constants import *
+from .utils import *
+from .rfc3339 import timestamptostr as rfc_time
+
+class BaseObject(object):
+    keys = []
+    defaults = {}
+
+    def __init__(self, parent=None, **kwargs):
+        self.parent = parent
+        self.attrib = {}
+        for key in kwargs:
+            if not key in self.keys:
+                raise KeyError, "Unexpected key {}".format(key)
+            self.attrib[key] = kwargs[key]
+
+    def __getitem__(self, key):
+        return self.attrib.get(key, None)
+
+    def __setitem__(self, key, value):
+        if not key in self.keys:
+            raise KeyError, "Unexpected key {}".format(key)
+        self.attrib[key] = value
+
+    def set_time(self, key, value):
+        assert type(value) in [int, float]
+        self[key] = rfc_time(value)
+
