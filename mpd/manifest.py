71bf36d36671e3eeb8a69a5d98f01492d006ced5
Martastain <martas@imm.cz>
2017-01-30 12:08:47 +0100

    add period updated

--- mpd/manifest.py
+++ mpd/manifest.py
@@ -6,7 +6,21 @@ from .objects import *
 __all__ = ["MPD"]
 
 class MPD(BaseObject):
-    keys = ["id", "profiles", "type", "availabilityStartTime", "availabilityEndTime", "publishTime", "mediaPresentationDuration", "minimumUpdatePeriod", "minBufferTime", "timeShiftBufferDepth", "suggestedPresentationDelay", "maxSegmentDuration", "maxSubsegmentDuration"]
+    keys = [
+            "id",
+            "profiles",
+            "type",
+            "availabilityStartTime",
+            "availabilityEndTime",
+            "publishTime",
+            "mediaPresentationDuration",
+            "minimumUpdatePeriod",
+            "minBufferTime",
+            "timeShiftBufferDepth",
+            "suggestedPresentationDelay",
+            "maxSegmentDuration",
+            "maxSubsegmentDuration"
+        ]
 
     defaults = {
             "xmlns:xsi" : MPD_XMLNS_XSI,
@@ -28,45 +42,23 @@ class MPD(BaseObject):
         super(MPD, self).__init__(**kwargs)
         self.periods = []
 
-    def __getitem__(self, index):
-        return self.periods[index]
-
     def __iter__(self):
         return self.periods.__iter__()
 
     def add_period(self, **kwargs):
-        self.periods.append(Period(**kwargs))
+        if not "id" in kwargs:
+            kwargs["id"] = len(self.periods)
+        if not "start" in kwargs:
+            kwargs["start"] = "PT0S"
+            #TODO: add start as total duration of previous periods
+        self.periods.append(Period(self, **kwargs))
         return self.periods[-1]
 
     @property
     def xml(self):
         result = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n"
-        result += "<MPD {}>\n".format(self.mk_attrs())
+        content = []
         for period in self.periods:
-            result += indent(period.xml)
-        result += "</MPD>"
+            content.append(period.xml)
+        result += mk_xml_tag("MPD", self.attrib, content)
         return result
-
-
-
-
-#######################
-
-"""<?xml version="1.0" encoding="utf-8" ?>
-<MPD xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="urn:mpeg:dash:schema:mpd:2011" xsi:schemaLocation="urn:mpeg:dash:schema:mpd:2011 http://standards.iso.org/ittf/PubliclyAvailableStandards/MPEG-DASH_schema_files/DASH-MPD.xsd" type="dynamic" availabilityStartTime="{{NOW}}" timeShiftBufferDepth="PT10S" minimumUpdatePeriod="PT595H" maxSegmentDuration="PT5S" minBufferTime="PT1S" profiles="urn:mpeg:dash:profile:isoff-live:2011,urn:com:dashif:dash264">
-    <Period id="1" start="PT0S">
-
-            <AdaptationSet group="1" mimeType="audio/mp4" minBandwidth="128000" maxBandwidth="128000" segmentAlignment="true">
-              <Representation id="128kbps" bandwidth="128000" codecs="mp4a.40.2" audioSamplingRate="48000">
-                <SegmentTemplate duration="8000" media="test-a-128-$Number$.m4v" initialization="test-a-128-init.mp4" startNumber="{{NUMBER}}" liveEdgeNumber="{{NUMBER}}"/>
-              </Representation>
-            </AdaptationSet>
-
-            <AdaptationSet group="2" mimeType="video/mp4" segmentAlignment="true">
-              <Representation id="4000kbps,1080p" frameRate="25" bandwidth="4000000" codecs="avc1.42c028" width="1920" height="1080">
-                <SegmentTemplate duration="8000" media="test-v-4000-$Number$.m4v" initialization="test-v-4000-init.mp4" startNumber="{{NUMBER}}" liveEdgeNumber="{{NUMBER}}"/>
-              </Representation>
-            </AdaptationSet>
-
-    </Period>
-</MPD>"""
